cd958b0a33f8d6c04a56e29a550364f3616813a2
muky1 <muhamed.begic01@gmail.com>
2024-03-15 21:31:38 +0100

    Add db_config template file for connection parameters

---
+++ rest/config/db_config.template.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+DATABASE CONFIG TEMPLATE,
+POPULATE WITH CREDENTIALS
+TO CONNECT TO MYSQL SERVER
+*/
+define("DB_HOST", "hostname");
+define("DB_USER", "username");
+define("DB_PASS", "password");
+define("DB_NAME", "dbschema");
+?>
